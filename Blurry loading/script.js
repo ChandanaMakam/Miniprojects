195a2e68451654b23666482c111ad95744fe11bf
Chandana Makam <chandu.makam@gmail.com>
2023-06-15 15:56:56 +0200

    Accomplished

---
+++ Blurry loading/script.js
@@ -0,0 +1,23 @@
+const bg = document.querySelector(".bg");
+const text = document.querySelector(".loading-text");
+let counter = 0;
+function incrementCounter() {
+    
+    if (counter < 100) {
+        counter++; // Increment the counter value by 1
+        text.innerText = counter + `%`;
+
+         }
+       
+       else {
+        clearInterval(counter); // Stop the interval once the counter reaches 100
+        text.classList.add ("blur");
+        bg.classList.add("blur");
+       
+      }   
+  }
+
+ counter = setInterval(incrementCounter, 30);
+ 
+    
+  
