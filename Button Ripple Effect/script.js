2957227b4231bd2bce5eef454f98baa36c3d1b18
Chandana Makam <chandu.makam@gmail.com>
2023-06-20 22:16:37 +0200

    ripple effect is working

--- Button Ripple Effect/script.js
+++ Button Ripple Effect/script.js
@@ -1,5 +1,5 @@
 const button = document.querySelector("button");
-button.addEventListener("click", e => {
+button.addEventListener("click", function (e) {
     console.log("check");
     const x = e.clientX;
     const y = e.clientY;
@@ -9,7 +9,9 @@ button.addEventListener("click", e => {
     const yInside = y - buttonTop;
     const circle = document.createElement("span");
     circle.classList.add ("circle");
-    document.append("circle");
+    circle.style.top = yInside +'px';
+    circle.style.left = xInside+'px';
+     this.appendChild(circle);
     setTimeout(() => circle.remove(), 500);
 
 
