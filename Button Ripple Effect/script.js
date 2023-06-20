ec24b3745046c3d476c7d0a511758cc46059a8c1
Chandana Makam <chandu.makam@gmail.com>
2023-06-20 22:19:18 +0200

    polished

--- Button Ripple Effect/script.js
+++ Button Ripple Effect/script.js
@@ -1,6 +1,5 @@
 const button = document.querySelector("button");
 button.addEventListener("click", function (e) {
-    console.log("check");
     const x = e.clientX;
     const y = e.clientY;
     const buttonTop = e.target.offsetTop;
